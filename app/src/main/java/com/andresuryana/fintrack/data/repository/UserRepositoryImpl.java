5e68c26e14ad9496b4a2d5c6614bbf2d87d9f780
AndreSuryana <Andresuryana17@gmail.com>
2023-06-07 16:15:02 +0700

    add try catch block to each function

--- app/src/main/java/com/andresuryana/fintrack/data/repository/UserRepositoryImpl.java
+++ app/src/main/java/com/andresuryana/fintrack/data/repository/UserRepositoryImpl.java
@@ -15,64 +15,72 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public void login(User user, AuthCallback callback) {
-        auth.signInWithEmailAndPassword(user.getEmail(), user.getPassword())
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        // Get firebase credentials
-                        FirebaseUser firebaseUser = auth.getCurrentUser();
-                        if (firebaseUser != null) {
-                            // Get user id, display name & user token
-                            user.setUserId(firebaseUser.getUid());
-                            user.setName(firebaseUser.getDisplayName());
-                            firebaseUser.getIdToken(true).addOnCompleteListener(tokenResult -> {
-                               if (tokenResult.isSuccessful()) {
-                                   user.setToken(tokenResult.getResult().getToken());
-                                   callback.onSuccess(user);
-                               }
-                            });
+        try {
+            auth.signInWithEmailAndPassword(user.getEmail(), user.getPassword())
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            // Get firebase credentials
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            if (firebaseUser != null) {
+                                // Get user id, display name & user token
+                                user.setUserId(firebaseUser.getUid());
+                                user.setName(firebaseUser.getDisplayName());
+                                firebaseUser.getIdToken(true).addOnCompleteListener(tokenResult -> {
+                                    if (tokenResult.isSuccessful()) {
+                                        user.setToken(tokenResult.getResult().getToken());
+                                        callback.onSuccess(user);
+                                    }
+                                });
+                            }
+                        } else {
+                            String errorMessage = task.getException() != null ? task.getException().getMessage() : "An error occurred";
+                            callback.onFailure(errorMessage);
                         }
-                    } else {
-                        String errorMessage = task.getException() != null ? task.getException().getMessage() : "An error occurred";
-                        callback.onFailure(errorMessage);
-                    }
-                });
+                    });
+        } catch (Exception e) {
+            callback.onFailure(e.getMessage());
+        }
     }
 
     @Override
     public void register(User user, AuthCallback callback) {
-        auth.createUserWithEmailAndPassword(user.getEmail(), user.getPassword())
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        FirebaseUser firebaseUser = auth.getCurrentUser();
-                        if (firebaseUser != null) {
-                            // Update user profile (display name)
-                            UserProfileChangeRequest userProfile = new UserProfileChangeRequest.Builder()
-                                    .setDisplayName(user.getName())
-                                    .build();
-                            firebaseUser.updateProfile(userProfile)
-                                    .addOnCompleteListener(updateTask -> {
-                                        if (!updateTask.isSuccessful()) {
-                                            String errorMessage = task.getException() != null ? task.getException().getMessage() : "An error occurred";
-                                            callback.onFailure(errorMessage);
+        try {
+            auth.createUserWithEmailAndPassword(user.getEmail(), user.getPassword())
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            if (firebaseUser != null) {
+                                // Update user profile (display name)
+                                UserProfileChangeRequest userProfile = new UserProfileChangeRequest.Builder()
+                                        .setDisplayName(user.getName())
+                                        .build();
+                                firebaseUser.updateProfile(userProfile)
+                                        .addOnCompleteListener(updateTask -> {
+                                            if (!updateTask.isSuccessful()) {
+                                                String errorMessage = task.getException() != null ? task.getException().getMessage() : "An error occurred";
+                                                callback.onFailure(errorMessage);
 
-                                            // Delete user if profile update failed
-                                            firebaseUser.delete();
-                                        }
-                                    });
+                                                // Delete user if profile update failed
+                                                firebaseUser.delete();
+                                            }
+                                        });
 
-                            // Get user id & token
-                            firebaseUser.getIdToken(true).addOnCompleteListener(tokenResult -> {
-                                if (tokenResult.isSuccessful()) {
-                                    user.setUserId(firebaseUser.getUid());
-                                    user.setToken(tokenResult.getResult().getToken());
-                                    callback.onSuccess(user);
-                                }
-                            });
+                                // Get user id & token
+                                firebaseUser.getIdToken(true).addOnCompleteListener(tokenResult -> {
+                                    if (tokenResult.isSuccessful()) {
+                                        user.setUserId(firebaseUser.getUid());
+                                        user.setToken(tokenResult.getResult().getToken());
+                                        callback.onSuccess(user);
+                                    }
+                                });
+                            }
+                        } else {
+                            String errorMessage = task.getException() != null ? task.getException().getMessage() : "An error occurred";
+                            callback.onFailure(errorMessage);
                         }
-                    } else {
-                        String errorMessage = task.getException() != null ? task.getException().getMessage() : "An error occurred";
-                        callback.onFailure(errorMessage);
-                    }
-                });
+                    });
+        } catch (Exception e) {
+            callback.onFailure(e.getMessage());
+        }
     }
 }
