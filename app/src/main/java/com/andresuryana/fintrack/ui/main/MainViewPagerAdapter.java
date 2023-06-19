d61ac40c25af705e3a3acd135879f6b1e73b37db
AndreSuryana <Andresuryana17@gmail.com>
2023-06-19 16:34:13 +0700

    fix bugs category fragment still visible

--- app/src/main/java/com/andresuryana/fintrack/ui/main/MainViewPagerAdapter.java
+++ app/src/main/java/com/andresuryana/fintrack/ui/main/MainViewPagerAdapter.java
@@ -6,7 +6,6 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import com.andresuryana.fintrack.ui.analytic.AnalyticFragment;
-import com.andresuryana.fintrack.ui.category.CategoryFragment;
 import com.andresuryana.fintrack.ui.dashboard.DashboardFragment;
 import com.andresuryana.fintrack.ui.profile.ProfileFragment;
 import com.andresuryana.fintrack.ui.transaction.TransactionFragment;
@@ -19,9 +18,8 @@ public class MainViewPagerAdapter extends FragmentStateAdapter {
     public static final int MENU_DASHBOARD = 0;
     public static final int MENU_TRANSACTIONS = 1;
     public static final int MENU_ANALYTICS = 2;
-    public static final int MENU_CATEGORIES = 3;
-    public static final int MENU_PROFILE = 4;
-    public static final int MENU_COUNT = 5;
+    public static final int MENU_PROFILE = 3;
+    public static final int MENU_COUNT = 4;
 
     private final List<Fragment> fragments = new ArrayList<>();
 
@@ -35,7 +33,6 @@ public class MainViewPagerAdapter extends FragmentStateAdapter {
         fragments.add(MENU_DASHBOARD, new DashboardFragment());
         fragments.add(MENU_TRANSACTIONS, new TransactionFragment());
         fragments.add(MENU_ANALYTICS, new AnalyticFragment());
-        fragments.add(MENU_CATEGORIES, new CategoryFragment());
         fragments.add(MENU_PROFILE, new ProfileFragment());
     }
 
